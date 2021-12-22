f27cb73a05ddf38f406eadcd36ec29c15e27ac22
lester <lester@tenacity.com.cn>
2021-12-22 14:32:56 +0800

    '组织架构联调'

--- src/pages/OrgManage/Organization/Organization.tsx
+++ src/pages/OrgManage/Organization/Organization.tsx
@@ -7,8 +7,8 @@ import React, { useEffect, useState } from 'react';
 import { Input, Tree, Modal as AntdModal, message } from 'antd';
 import classNames from 'classnames';
 import { setTitle, copy } from 'lester-tools';
-import { Icon, Modal } from 'src/components';
-import { queryCorpOrg } from 'src/apis/stationConfig';
+import { Icon, Modal, Empty } from 'src/components';
+import { queryDepartmentList, searchStaffAndDepart } from 'src/apis/organization';
 import StaffList from './StaffList/StaffList';
 import StaffDetail from './StaffDetail/StaffDetail';
 import SetLeader from './components/SetLeader';
@@ -16,17 +16,22 @@ import style from './style.module.less';
 
 const { Search } = Input;
 
+interface StaffItem {
+  staffId?: string;
+  staffName?: string;
+}
+
 interface OrganizationItem {
-  id?: string;
-  name?: string;
-  key?: string;
-  title?: string;
-  isParent?: boolean;
+  deptId?: string;
+  deptName?: string;
+  deptType?: number;
+  effCount?: boolean;
   isLeaf?: boolean;
   index?: number;
-  total?: number;
+  totalCount?: number;
   leaderId?: string;
-  isRoot?: boolean;
+  leaderName?: string;
+  total?: number;
   children?: OrganizationItem[];
 }
 
@@ -44,13 +49,12 @@ const Organization: React.FC = () => {
   const [showDepart, setShowDepart] = useState<boolean>(false);
   const [departmentVisible, setDepartmentVisible] = useState<boolean>(false);
   const [departmentName, setDepartmentName] = useState<string>('');
-  const [currentNode, setCurrentNode] = useState<OrganizationItem>({});
+  const [currentNode, setCurrentNode] = useState<OrganizationItem & StaffItem>({});
   const [deleteVisible, setDeleteVisible] = useState<boolean>(false);
   const [searchList, setSearchList] = useState<OrganizationItem[]>([]);
+  const [staffList, setStaffList] = useState<StaffItem[]>([]);
   const [displayType, setDisplayType] = useState<number>(0);
-  const [chooseNode, setChooseNode] = useState<OrganizationItem>({});
   const [leaderVisible, setLeaderVisible] = useState<boolean>(false);
-  const [currentDepartment, setCurrentDepartment] = useState<OrganizationItem>({});
 
   /**
    * 处理/计算左边位置
@@ -75,9 +79,14 @@ const Organization: React.FC = () => {
    * @param type
    */
   const moveData = (data: OrganizationItem[], key: string, type: string): OrganizationItem[] => {
-    const index: number = data.findIndex((item) => item.id === key);
+    const copyData = data.slice(0);
+    const isNewStaffDepart = Number(copyData[0].deptId) === -1;
+    let newStaffDepart: OrganizationItem[] = [];
+    if (isNewStaffDepart) {
+      newStaffDepart = copyData.splice(0, 1);
+    }
+    const index: number = copyData.findIndex((item) => item.deptId === key);
     if (index > -1) {
-      const copyData = data.slice(0);
       const temp = copyData[index];
       const nextIndex = type === 'up' ? index - 1 : index + 1;
       copyData[index] = {
@@ -92,32 +101,33 @@ const Organization: React.FC = () => {
         ...temp,
         index: nextIndex
       });
-      return copyData;
+      return newStaffDepart.concat(copyData);
     } else {
-      return data.map((item) => {
-        if (item.children && item.children.length > 0) {
-          return {
-            ...item,
-            children: moveData(item.children, key, type)
-          };
-        }
-        return item;
-      });
+      return newStaffDepart.concat(
+        copyData.map((item) => {
+          if (item.children && item.children.length > 0) {
+            return {
+              ...item,
+              children: moveData(item.children, key, type)
+            };
+          }
+          return item;
+        })
+      );
     }
   };
 
   /**
    * 格式化数据源
    * @param data
-   * @param isRoot
    */
-  const formatData = (data: OrganizationItem[], isRoot?: boolean) => {
+  const formatData = (data: OrganizationItem[]) => {
+    const isNewStaffDepart = Number(data[0].deptId) === -1;
     return data.map((item, index) => ({
       ...item,
-      isLeaf: !item.isParent,
-      index,
-      total: data.length,
-      isRoot
+      index: isNewStaffDepart ? index - 1 : index,
+      total: isNewStaffDepart ? data.length - 1 : data.length,
+      children: []
     }));
     /* if (resItem.children && resItem.children.length > 0) {
       resItem.children = formatData(resItem.children);
@@ -132,7 +142,7 @@ const Organization: React.FC = () => {
    */
   const updateData = (list: OrganizationItem[], key: string, children: OrganizationItem[]): OrganizationItem[] => {
     return list.map((item) => {
-      if (item.id === key) {
+      if (item.deptId === key) {
         return {
           ...item,
           children: formatData(children)
@@ -155,7 +165,7 @@ const Organization: React.FC = () => {
    */
   const onLoadData = async ({ key, children }: any): Promise<void> => {
     if (!children || children?.length === 0) {
-      const res: any = await queryCorpOrg({ parentId: key });
+      const res: any = await queryDepartmentList({ parentId: key });
       if (res) {
         setOrganization((data) => updateData(data, key, res));
       }
@@ -163,13 +173,32 @@ const Organization: React.FC = () => {
   };
 
   /**
+   * 更新节点信息
+   * @param data
+   * @param node
+   */
+  const updateNodeInfo = (data: OrganizationItem[], node: OrganizationItem): OrganizationItem[] => {
+    return data.map((item) => {
+      if (item.deptId === node.deptId) {
+        return node;
+      }
+      if (item.children && item.children.length > 0) {
+        return {
+          ...item,
+          children: updateNodeInfo(item.children, node)
+        };
+      }
+      return item;
+    });
+  };
+
+  /**
    * 获取组织架构初始数据
    */
   const initCorpOrgData = async () => {
-    const res: any = await queryCorpOrg({});
+    const res: any = await queryDepartmentList({});
     if (res && res.length > 0) {
-      res[0].children = [];
-      setOrganization(formatData(res, true));
+      setOrganization(formatData(res));
     }
   };
 
@@ -177,7 +206,7 @@ const Organization: React.FC = () => {
    * 删除部门
    */
   const delDepartment = () => {
-    if (!currentNode.isParent) {
+    if (currentNode.isLeaf) {
       AntdModal.confirm({
         title: '提示',
         content: '确定删除？',
@@ -192,25 +221,24 @@ const Organization: React.FC = () => {
    * 搜索
    * @param val
    */
-  const onSearch = (val: string) => {
+  const onSearch = async (val: string) => {
     if (val) {
-      setDisplayType(1);
-      setSearchList([
-        {
-          id: '123',
-          name: '龙春表'
-        },
-        {
-          id: '456',
-          name: '林堞雅'
+      const res: any = await searchStaffAndDepart({ keyWords: val });
+      if (res) {
+        const departList = res.deptList || [];
+        const staffList = res.staffList || [];
+        setSearchList(departList);
+        setStaffList(staffList);
+        if (departList.length > 0) {
+          setCurrentNode(departList[0]);
+        } else if (staffList.length > 0) {
+          setCurrentNode(staffList[0]);
         }
-      ]);
-      setChooseNode({
-        id: '123',
-        name: '龙春表'
-      });
+      }
+      setDisplayType(1);
     } else {
       setDisplayType(0);
+      setCurrentNode({});
     }
   };
 
@@ -232,7 +260,7 @@ const Organization: React.FC = () => {
 
   return (
     <div className={style.wrap}>
-      <section className={classNames(style.left, 'scroll-strip')}>
+      <section className={style.left}>
         <div className={style.inputWrap}>
           <Search
             placeholder="搜索成员、部门"
@@ -240,10 +268,13 @@ const Organization: React.FC = () => {
             enterButton={<Icon className={style.searchIcon} name="icon_common_16_seach" />}
           />
         </div>
-        <div style={{ display: displayType === 0 ? 'block' : 'none' }}>
+        <div
+          style={{ display: displayType === 0 ? 'block' : 'none' }}
+          className={classNames(style.treeContainer, 'scroll-strip')}
+        >
           <Tree
             className={style.treeWrap}
-            fieldNames={{ title: 'name', key: 'id' }}
+            fieldNames={{ title: 'deptName', key: 'deptId' }}
             blockNode
             expandedKeys={expandIds}
             onExpand={(keys) => setExpandIds(keys)}
@@ -251,44 +282,63 @@ const Organization: React.FC = () => {
             loadData={onLoadData}
             titleRender={(node) => (
               <div className={style.nodeItem}>
-                {node.name}({node.isRoot ? '78/80' : '60'})
-                <Icon
-                  className={style.dotIcon}
-                  name="diandian"
-                  onClick={(e) => {
-                    console.log(node);
-                    e.stopPropagation();
-                    handlePosition(e.clientX, e.clientY);
-                    setShowDepart(true);
-                    setCurrentNode(node);
-                  }}
-                />
+                {node.deptName}({node.deptType === 1 ? `${node.effCount}/${node.totalCount}` : node.effCount})
+                {Number(node.deptId) > -1 && (
+                  <Icon
+                    className={style.dotIcon}
+                    name="diandian"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handlePosition(e.clientX, e.clientY);
+                      setShowDepart(true);
+                      setCurrentNode(node);
+                    }}
+                  />
+                )}
               </div>
             )}
-            selectedKeys={[currentDepartment.id || '']}
+            selectedKeys={[currentNode.deptId || '']}
             onSelect={(selectedKeys, { selectedNodes }) => {
               if (selectedNodes.length > 0) {
-                setCurrentDepartment(selectedNodes[0]);
+                setCurrentNode(selectedNodes[0]);
               }
             }}
           />
         </div>
         <ul style={{ display: displayType === 1 ? 'block' : 'none' }} className={style.searchList}>
+          {searchList.length === 0 && staffList.length === 0 && <Empty />}
           {searchList.map((item: OrganizationItem) => (
             <li
-              key={item.id}
+              key={item.deptId}
               className={classNames(style.searchItem, {
-                [style.active]: item.id === chooseNode.id
+                [style.active]: item.deptId === currentNode.deptId
               })}
-              onClick={() => setChooseNode(item)}
+              onClick={() => setCurrentNode(item)}
             >
-              {item.name}
+              {item.deptName}
+            </li>
+          ))}
+          {staffList.map((item: StaffItem) => (
+            <li
+              key={item.staffId}
+              className={classNames(style.searchItem, {
+                [style.active]: item.staffId === currentNode.staffId
+              })}
+              onClick={() => setCurrentNode(item)}
+            >
+              {item.staffName}
             </li>
           ))}
         </ul>
       </section>
       <div className={style.right}>
-        {displayType === 0 ? <StaffList departmentId={currentDepartment.id!} /> : <StaffDetail />}
+        {currentNode.staffId
+          ? (
+          <StaffDetail staffId={currentNode.staffId} />
+            )
+          : (
+          <StaffList departmentId={currentNode.deptId!} />
+            )}
       </div>
       <ul
         style={{ ...position, transform: `scale(${showDepart ? 1 : 0})` }}
@@ -297,25 +347,25 @@ const Organization: React.FC = () => {
       >
         <li
           className={style.operationItem}
-          title={currentNode.id}
+          title={currentNode.deptId}
           onClick={() => {
-            copy(currentNode.id!, false);
+            copy(currentNode.deptId!, false);
             message.success('部门id已复制');
           }}
         >
-          部门ID：{currentNode.id}
+          部门ID：{currentNode.deptId}
         </li>
         <li className={style.operationItem} onClick={() => setDepartmentVisible(true)}>
           添加子部门
         </li>
         <li
           className={classNames(style.operationItem, {
-            [style.disabled]: currentNode.isRoot
+            [style.disabled]: currentNode.deptType !== 0
           })}
           onClick={() => {
-            if (!currentNode.isRoot) {
+            if (currentNode.deptType === 0) {
               setDepartmentVisible(true);
-              setDepartmentName(currentNode.name!);
+              setDepartmentName(currentNode.deptName!);
             }
           }}
         >
@@ -326,9 +376,9 @@ const Organization: React.FC = () => {
         </li>
         <li
           className={classNames(style.operationItem, {
-            [style.disabled]: currentNode.isParent
+            [style.disabled]: !currentNode.isLeaf || currentNode.deptType !== 0
           })}
-          onClick={() => !currentNode.isParent && setDeleteVisible(true)}
+          onClick={() => currentNode.isLeaf && currentNode.deptType === 0 && setDeleteVisible(true)}
         >
           删除
         </li>
@@ -336,7 +386,7 @@ const Organization: React.FC = () => {
           className={classNames(style.operationItem, {
             [style.disabled]: currentNode.index === 0
           })}
-          onClick={() => currentNode.index! > 0 && setOrganization(moveData(organization, currentNode.id!, 'up'))}
+          onClick={() => currentNode.index! > 0 && setOrganization(moveData(organization, currentNode.deptId!, 'up'))}
         >
           上移
         </li>
@@ -346,7 +396,7 @@ const Organization: React.FC = () => {
           })}
           onClick={() =>
             currentNode.index! < (currentNode.total || 0) - 1 &&
-            setOrganization(moveData(organization, currentNode.id!, 'down'))
+            setOrganization(moveData(organization, currentNode.deptId!, 'down'))
           }
         >
           下移
@@ -381,11 +431,23 @@ const Organization: React.FC = () => {
           <div className={style.textCenter}>确定删除？</div>
         </Modal>
         <SetLeader
-          chooseIds={[currentNode.id!]}
+          deptId={currentNode.deptId!}
+          deptType={currentNode.deptType!}
+          leaderInfo={{
+            staffId: currentNode.leaderId,
+            staffName: currentNode.leaderName
+          }}
           visible={leaderVisible}
           onClose={() => setLeaderVisible(false)}
-          onOk={(ids) => {
-            console.log(ids);
+          onOk={(leaderInfo) => {
+            const newNode: OrganizationItem = {
+              ...currentNode,
+              leaderId: leaderInfo.staffId,
+              leaderName: leaderInfo.staffName
+            };
+            setCurrentNode(newNode);
+            setOrganization(updateNodeInfo(organization, newNode));
+            setLeaderVisible(false);
           }}
         />
       </div>
