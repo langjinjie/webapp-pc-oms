6e52e8d9670bbb75085912caef9c7ac25c9c9ec6
jeff lang <l@tenacity.com.cn>
2022-01-18 18:50:32 +0800

    后管端-销售宝典话术模板支持pdf及pdf最大上线改为50m

--- src/pages/SalesCollection/SpeechManage/Config.tsx
+++ src/pages/SalesCollection/SpeechManage/Config.tsx
@@ -23,7 +23,7 @@ export const speechContentTypes = [
   { id: 10, name: 'PDF' }
 ];
 export const excelDemoUrl =
-  'https://insure-prod-server-1305111576.cos.ap-guangzhou.myqcloud.com/file/smart/smart_content_import_template.xlsx';
+  'https://insure-prod-server-1305111576.cos.ap-guangzhou.myqcloud.com/file/smart/%E5%90%8E%E7%AE%A1%E7%AB%AF%E8%AF%9D%E6%9C%AF%E6%A8%A1%E6%9D%BF.xlsx';
 
 export const statusOptions = [
   { id: 0, name: '待上架' },
