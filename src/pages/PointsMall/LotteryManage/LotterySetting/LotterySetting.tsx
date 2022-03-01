cf5da9e5f8234a6a84851d6201ac1b8180ae5223
jeff lang <langjinjie@tenacity.com.cn>
2022-03-01 18:02:11 +0800

    积分商城抽奖管理页面迭代开发

---
+++ src/pages/PointsMall/LotteryManage/LotterySetting/LotterySetting.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './style.module.less';
+
+const LotterySetting: React.FC = () => {
+  return <div className={style.wrap}>LotterySetting</div>;
+};
+export default LotterySetting;
