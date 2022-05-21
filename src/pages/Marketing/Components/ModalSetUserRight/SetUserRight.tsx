372d7ecfa7d45082fbb4962303611d0164b19742
yu yadong <yuyd@tenacity.com.cn>
2022-05-22 01:44:59 +0800

    feat: 配置可见范围实现

--- src/pages/Marketing/Components/ModalSetUserRight/SetUserRight.tsx
+++ src/pages/Marketing/Components/ModalSetUserRight/SetUserRight.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { getUserGroup } from 'src/apis/marketing';
 import { NgModal } from 'src/components';
 import UserGroupModal from './UserGroupModal';
+import UserOrgModal from './UserOrgModal';
 
 interface SetUserRightProps extends Omit<React.ComponentProps<typeof NgModal>, 'onOk'> {
   title?: string;
@@ -16,16 +17,13 @@ export const SetUserRight: React.FC<SetUserRightProps> = ({ title, groupId, visi
   const [formValues, setFormValues] = useState<{ isSet: number; groupType: number; group: any }>({
     isSet: 0,
     groupType: 1,
-    group: {
-      groupType: 1,
-      info: {}
-    }
+    group: {}
   });
   const handleSubmit = () => {
     rightForm.validateFields().then((values) => {
       const { group, isSet } = values;
       console.log({ group, isSet });
-      onOk?.({ groupId: group?.info.groupId, isSet });
+      onOk?.({ groupId: group.groupId, isSet });
     });
   };
   const getGroup = async () => {
@@ -36,40 +34,51 @@ export const SetUserRight: React.FC<SetUserRightProps> = ({ title, groupId, visi
         groupType,
         isSet: 1,
         group: {
-          groupType: groupType,
-          info: { ...res.groupInfo, groupId, ...res.orgInfo }
+          ...res.groupInfo,
+          groupId,
+          ...res.orgInfo
         }
       });
       setFormValues((formValues) => ({
         ...formValues,
         isSet: 1,
         groupType,
-        group: { groupType, info: { ...res.groupInfo, groupId, ...res.orgInfo } }
+        group: { ...res.groupInfo, groupId, ...res.orgInfo }
       }));
+    } else {
+      setFormValues({
+        isSet: 0,
+        groupType: 1,
+        group: {}
+      });
+      rightForm.resetFields();
     }
   };
   useEffect(() => {
     getGroup();
-  }, [groupId]);
+  }, [groupId, visible]);
   const onValuesChange = (changeValues: any, values: any) => {
+    console.log('111', values);
     const { groupType = 1, isSet, group } = values;
-    const groupRes = changeValues.groupType ? { info: {}, groupType } : { ...group, groupType };
-    console.log('group: ', groupRes);
-    setFormValues((formValues) => ({ ...formValues, isSet, group: groupRes }));
+
     rightForm.setFieldsValue({
-      group: groupRes
+      group: changeValues.groupType ? {} : group
     });
+    setFormValues((formValues) => ({
+      ...formValues,
+      isSet,
+      group: changeValues.groupType ? {} : group,
+      groupType
+    }));
   };
 
   const handleCancel = (e: any) => {
-    rightForm.resetFields();
     setFormValues({
       isSet: 0,
       groupType: 1,
-      group: {
-        groupType: 1
-      }
+      group: {}
     });
+    rightForm.resetFields();
     onCancel?.(e);
   };
 
@@ -107,9 +116,17 @@ export const SetUserRight: React.FC<SetUserRightProps> = ({ title, groupId, visi
                     <Radio value={2}>按照组织架构选择</Radio>
                   </Radio.Group>
                 </Form.Item>
-                <Form.Item name={'group'}>
-                  <UserGroupModal />
-                </Form.Item>
+                {formValues.groupType === 1
+                  ? (
+                  <Form.Item name={'group'}>
+                    <UserGroupModal />
+                  </Form.Item>
+                    )
+                  : (
+                  <Form.Item name={'group'}>
+                    <UserOrgModal />
+                  </Form.Item>
+                    )}
                 <Form.Item>
                   上次选择的可见范围为：
                   {formValues.groupType === 1
@@ -119,7 +136,7 @@ export const SetUserRight: React.FC<SetUserRightProps> = ({ title, groupId, visi
                 <Form.Item>
                   <span>
                     截止目前时间：此用户组共计人数：
-                    {formValues.group?.info?.staffNum || 0}人
+                    {formValues.group?.staffNum || 0}人
                   </span>
                   <Button type="link">查看人员</Button>
                 </Form.Item>
--- src/pages/Marketing/Components/ModalSetUserRight/UserGroupModal.tsx
+++ src/pages/Marketing/Components/ModalSetUserRight/UserGroupModal.tsx
@@ -1,46 +1,20 @@
 import { Button, Tag } from 'antd';
-import React, { useMemo, useState } from 'react';
-import { createSingleGroup } from 'src/apis/marketing';
+import React, { useState } from 'react';
 import { NgModal } from 'src/components';
 import UserGroup from 'src/pages/OrgManage/UserGroup/UserGroup';
-import { OrganizationalTree } from 'src/pages/RoleManage/components';
 
 interface UserGroupModalProps extends Omit<React.ComponentProps<typeof NgModal>, 'onOk'> {
   onChange?: (item: any) => void;
   value?: { groupType: number; [prop: string]: any };
 }
 const UserGroupModal: React.FC<UserGroupModalProps> = ({ onChange, value: propValue }) => {
-  console.log({ propValue });
   const [value, setValue] = useState<any>();
   const [visibleUserGroup, setVisibleUserGroup] = useState(false);
-  useMemo(() => {
-    setValue(null);
-  }, [propValue?.groupType]);
 
   const handleChange = (value: any) => {
-    setValue({
-      groupType: propValue?.groupType,
-      info: value
-    });
+    setValue(value);
   };
-  const handleOnOK = async (values?: any[]) => {
-    if (values) {
-      const staffList: any[] = [];
-      const deptList: any[] = [];
-      values.forEach((item: any) => {
-        if (item.staffId) {
-          staffList.push({ staffId: item.staffId });
-        } else {
-          deptList.push({ deptId: item.deptId });
-        }
-      });
-      const res = await createSingleGroup({ staffList, deptList });
-      setVisibleUserGroup(false);
-      return onChange?.({
-        groupType: propValue?.groupType,
-        info: { orgInfo: values, ...res }
-      });
-    }
+  const handleOnOK = async () => {
     onChange?.(value);
     setVisibleUserGroup(false);
   };
@@ -50,39 +24,17 @@ const UserGroupModal: React.FC<UserGroupModalProps> = ({ onChange, value: propVa
   };
   return (
     <>
-      {propValue?.groupType === 1 && value?.info?.groupName ? <Tag>{value.info.groupName}</Tag> : null}
-      {propValue?.groupType === 2 && value?.info && value.info.length > 0
-        ? (
-        <>
-          {value?.info?.map((item: any, index: number) => {
-            return <Tag key={index}>{item.staffName || item.deptName}</Tag>;
-          })}
-        </>
-          )
-        : null}
-      {propValue?.groupType === 1
-        ? (
-        <NgModal
-          width={'940px'}
-          visible={visibleUserGroup}
-          title="选择用户组"
-          onCancel={() => setVisibleUserGroup(false)}
-          onOk={() => handleOnOK()}
-        >
-          <UserGroup change={handleChange}></UserGroup>
-        </NgModal>
-          )
-        : (
-        <OrganizationalTree
-          showStaff
-          params={{
-            visible: visibleUserGroup
-          }}
-          onCancel={() => setVisibleUserGroup(false)}
-          onOk={handleOnOK}
-          onChange={handleChange}
-        />
-          )}
+      {propValue?.groupName ? <Tag>{propValue.groupName}</Tag> : null}
+      <NgModal
+        width={'940px'}
+        visible={visibleUserGroup}
+        title="选择用户组"
+        onCancel={() => setVisibleUserGroup(false)}
+        onOk={() => handleOnOK()}
+      >
+        <UserGroup change={handleChange}></UserGroup>
+      </NgModal>
+
       <Button shape="round" onClick={handleShowSelectModal}>
         修改可见范围
       </Button>
