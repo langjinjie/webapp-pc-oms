e9da10b7937df69c1137f2ef6e4eb01f0869f0da
jeff lang <langjinjie@tenacity.com.cn>
2022-08-25 14:12:10 +0800

    在职离职继承查询时间参数处理

--- src/pages/StaffManage/components/DistributeList/DistributeList.tsx
+++ src/pages/StaffManage/components/DistributeList/DistributeList.tsx
@@ -71,13 +71,13 @@ const DistributeList: React.FC<IDistributeListProps> = ({ distributeLisType }) =
     let addBeginTime = '';
     let addEndTime = '';
     if (addTime) {
-      addBeginTime = addTime[0].stafOf('days').format('YYYY-MM-DD HH:mm:ss');
+      addBeginTime = addTime[0].startOf('days').format('YYYY-MM-DD HH:mm:ss');
       addEndTime = addTime[1].endOf('days').format('YYYY-MM-DD HH:mm:ss');
     }
     let takeoverBeginTime = '';
     let takeoverEndTime = '';
     if (takeoverTime) {
-      takeoverBeginTime = takeoverTime[0].stafOf('days').format('YYYY-MM-DD HH:mm:ss');
+      takeoverBeginTime = takeoverTime[0].startOf('days').format('YYYY-MM-DD HH:mm:ss');
       takeoverEndTime = takeoverTime[1].endOf('days').format('YYYY-MM-DD HH:mm:ss');
     }
     setFormValue({
