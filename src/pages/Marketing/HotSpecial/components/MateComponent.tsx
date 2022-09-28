428757c651b58ef60a9784dbd7ee9a3800fa691b
jeff lang <langjinjie@tenacity.com.cn>
2022-09-28 11:13:16 +0800

    热门专题配置内容支持排序

--- src/pages/Marketing/HotSpecial/components/MateComponent.tsx
+++ src/pages/Marketing/HotSpecial/components/MateComponent.tsx
@@ -6,7 +6,8 @@ import { Icon } from 'tenacity-ui';
 import { ArticleSelectComponent } from './ArticleSelectComponent';
 import { PosterSelectComponent } from './PosterSelectComponent';
 import { ProductSelectComponent } from './ProductSelectComponent';
-
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { Drag, Drop, DropChild } from 'src/components/drag-and-drop';
 import style from './style.module.less';
 import { ActivitySelectComponent } from './ActivitySelectComponent';
 
@@ -34,6 +35,22 @@ export const MeatComponent: React.FC<MeatComponentProps> = ({ type, value, onCha
     setSelectRows([...res, ...filterRows]);
     onChange?.([...res, ...filterRows]);
     setSelectedRowKeys(keys);
+  };
+
+  // 重新记录数组顺序
+  const reorder = (list: any[], startIndex: number, endIndex: number) => {
+    const result = [...list];
+    // 删除并记录 删除元素
+    const [removed] = result.splice(startIndex, 1);
+    // 将原来的元素添加进数组
+    result.splice(endIndex, 0, removed);
+    console.log('result', result);
+    return result;
+  };
+
+  // 拖拽结束
+  const onDragEnd = async ({ source, destination }: DropResult) => {
+    destination && onChange?.(reorder(value as any[], source.index, destination.index));
   };
 
   useEffect(() => {
@@ -80,13 +97,27 @@ export const MeatComponent: React.FC<MeatComponentProps> = ({ type, value, onCha
           <h3 className="pb20">已选择</h3>
           <div className={classNames(style.panelWrap, style.tagWrap)}>
             <div className={classNames(style.marketingWarp)}>
-              {isArray(value) &&
-                (value as any[])?.map((row: any, index) => (
-                  <div className={classNames(style.customTag)} key={row.itemId + index}>
-                    <span>{row.itemName}</span>
-                    <Icon className={style.closeIcon} name="biaoqian_quxiao" onClick={() => removeItem(index)}></Icon>
-                  </div>
-                ))}
+              {isArray(value) && (
+                <DragDropContext onDragEnd={onDragEnd}>
+                  {/* direction代表拖拽方向  默认垂直方向  水平方向:horizontal */}
+                  <Drop type={'ROW'} droppableId={'kanban'}>
+                    <DropChild>
+                      {(value as any[])?.map((row: any, index) => (
+                        <Drag index={index} draggableId={index + 'draggableId'} key={'draggableId' + index}>
+                          <div className={classNames(style.customTag)} key={row.itemId + index}>
+                            <span>{row.itemName}</span>
+                            <Icon
+                              className={style.closeIcon}
+                              name="biaoqian_quxiao"
+                              onClick={() => removeItem(index)}
+                            ></Icon>
+                          </div>
+                        </Drag>
+                      ))}
+                    </DropChild>
+                  </Drop>
+                </DragDropContext>
+              )}
             </div>
           </div>
         </div>
