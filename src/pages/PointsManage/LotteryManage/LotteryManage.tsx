70e21c3e42120272c62d2e660fbe9c856bbdf04d
jeff lang <langjinjie@tenacity.com.cn>
2022-10-09 15:37:20 +0800

    抽奖管理样式优化

--- src/pages/PointsManage/LotteryManage/LotteryManage.tsx
+++ src/pages/PointsManage/LotteryManage/LotteryManage.tsx
@@ -5,14 +5,16 @@ import LotterySetting from './LotterySetting/LotterySetting';
 import style from './style.module.less';
 
 const LotteryManage: React.FC = () => {
-  const tabsNavList = ['抽奖设置', '大奖发放'];
-  const tabsComponentsList = [<LotterySetting key={0} />, <ProvidePhone key={1} />];
+  const tabsNavList = [
+    { key: 0, name: '抽奖设置', component: <LotterySetting key={0} /> },
+    { key: 1, name: '大奖发放', component: <ProvidePhone key={1} /> }
+  ];
   return (
     <div className={style.wrap}>
-      <Tabs defaultActiveKey="0" animated={true}>
-        {tabsNavList.map((item, index) => (
-          <Tabs.TabPane tab={item} key={index}>
-            {tabsComponentsList[index]}
+      <Tabs defaultActiveKey="0">
+        {tabsNavList.map((item) => (
+          <Tabs.TabPane tab={item.name} key={item.key}>
+            {item.component}
           </Tabs.TabPane>
         ))}
       </Tabs>
