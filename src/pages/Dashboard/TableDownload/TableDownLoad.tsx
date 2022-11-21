26b5551f4583a7c1b594f25a851807216c96fd93
jeff lang <langjinjie@tenacity.com.cn>
2022-11-21 17:37:16 +0800

    feat：报表下载增加筛选项

--- src/pages/Dashboard/TableDownload/TableDownLoad.tsx
+++ src/pages/Dashboard/TableDownload/TableDownLoad.tsx
@@ -1,4 +1,4 @@
-import { Button, DatePicker, Form, message, PaginationProps, Select } from 'antd';
+import { Button, DatePicker, Form, message, PaginationProps, Select, Row } from 'antd';
 import moment, { Moment } from 'moment';
 import React, { useEffect, useState } from 'react';
 import {
@@ -10,6 +10,7 @@ import {
 import { AuthBtn, NgTable } from 'src/components';
 import { exportFile } from 'src/utils/base';
 import { columns, fileProps } from './Config';
+import style from './style.module.less';
 
 const TableDownLoad: React.FC = () => {
   const [pagination, setPagination] = useState<PaginationProps>({
@@ -23,6 +24,7 @@ const TableDownLoad: React.FC = () => {
   const [dataSource, setDataSource] = useState<fileProps[]>([]);
   const [templeList, setTempleList] = useState<{ templateName: string; tmpId: string; type: 0 | 1 }[]>([]);
   const [templeType, setTempleType] = useState<0 | 1>(0);
+  const [showSelect, setShowSelect] = useState(false);
 
   const [form] = Form.useForm();
 
@@ -52,6 +54,7 @@ const TableDownLoad: React.FC = () => {
   const selectOnChange = (value: string) => {
     const templeType = templeList.find((findItem) => findItem.tmpId === value)?.type || 0;
     setTempleType(templeType);
+    setShowSelect(true);
     if (templeType === 1) {
       form.setFieldsValue({ dateRange: null });
     }
@@ -86,25 +89,151 @@ const TableDownLoad: React.FC = () => {
   return (
     <div className="container">
       <AuthBtn path="/create">
-        <Form form={form} layout="inline" onFinish={createFile}>
-          <Form.Item label="报表类别" name="tmpId" rules={[{ required: true }]}>
-            <Select placeholder="请选择报表类别" allowClear onChange={selectOnChange}>
-              {templeList.map((mapItem) => (
-                <Select.Option key={mapItem.tmpId} value={mapItem.tmpId}>
-                  {mapItem.templateName}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item label="日期" name="dateRange" rules={[{ required: templeType === 0 }]}>
-            {/* type为1 不能选择时间 */}
-            <DatePicker.RangePicker disabled={templeType === 1} allowClear />
-          </Form.Item>
-          <Form.Item extra="备注：点击生成报表按钮后，若列表还在处理中状态，请稍后手动刷新页面试试。">
-            <Button type="primary" shape="round" htmlType="submit">
-              生成报表
-            </Button>
-          </Form.Item>
+        <Form form={form} layout="inline" className={style.form} onFinish={createFile}>
+          <Row wrap>
+            <Form.Item label="报表类别" name="tmpId" rules={[{ required: true }]}>
+              <Select placeholder="请选择报表类别" allowClear onChange={selectOnChange}>
+                {templeList.map((mapItem) => (
+                  <Select.Option key={mapItem.tmpId} value={mapItem.tmpId}>
+                    {mapItem.templateName}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+            <Form.Item label="日期" name="dateRange" rules={[{ required: templeType === 0 }]}>
+              {/* type为1 不能选择时间 */}
+              <DatePicker.RangePicker disabled={templeType === 1} allowClear />
+            </Form.Item>
+            <Form.Item extra="备注：点击生成报表按钮后，若列表还在处理中状态，请稍后手动刷新页面试试。">
+              <Button type="primary" shape="round" htmlType="submit">
+                生成报表
+              </Button>
+            </Form.Item>
+          </Row>
+          {showSelect && (
+            <Row wrap>
+              <Form.Item label="销售中心" name="name1" initialValue="total">
+                <Select
+                  allowClear
+                  mode="multiple"
+                  style={{ width: '200px' }}
+                  options={[
+                    {
+                      value: 'total',
+                      label: '全部'
+                    },
+                    {
+                      value: 'jack',
+                      label: 'Jack'
+                    },
+                    {
+                      value: 'lucy',
+                      label: 'Lucy'
+                    },
+                    {
+                      value: 'disabled',
+                      disabled: true,
+                      label: 'Disabled'
+                    },
+                    {
+                      value: 'Yiminghe',
+                      label: 'yiminghe'
+                    }
+                  ]}
+                ></Select>
+              </Form.Item>
+              <Form.Item label="销售大区" name="name2" initialValue="total">
+                <Select
+                  allowClear
+                  mode="multiple"
+                  style={{ width: '200px' }}
+                  options={[
+                    {
+                      value: 'total',
+                      label: '全部'
+                    },
+                    {
+                      value: 'jack',
+                      label: 'Jack'
+                    },
+                    {
+                      value: 'lucy',
+                      label: 'Lucy'
+                    },
+                    {
+                      value: 'disabled',
+                      disabled: true,
+                      label: 'Disabled'
+                    },
+                    {
+                      value: 'Yiminghe',
+                      label: 'yiminghe'
+                    }
+                  ]}
+                ></Select>
+              </Form.Item>
+              <Form.Item label="销售区域" name="name3" initialValue="total">
+                <Select
+                  allowClear
+                  mode="multiple"
+                  style={{ width: '200px' }}
+                  options={[
+                    {
+                      value: 'total',
+                      label: '全部'
+                    },
+                    {
+                      value: 'jack',
+                      label: 'Jack'
+                    },
+                    {
+                      value: 'lucy',
+                      label: 'Lucy'
+                    },
+                    {
+                      value: 'disabled',
+                      disabled: true,
+                      label: 'Disabled'
+                    },
+                    {
+                      value: 'Yiminghe',
+                      label: 'yiminghe'
+                    }
+                  ]}
+                ></Select>
+              </Form.Item>
+              <Form.Item label="销售团队" name="name4" initialValue="total">
+                <Select
+                  allowClear
+                  mode="multiple"
+                  style={{ width: '200px' }}
+                  options={[
+                    {
+                      value: 'total',
+                      label: '全部'
+                    },
+                    {
+                      value: 'jack',
+                      label: 'Jack'
+                    },
+                    {
+                      value: 'lucy',
+                      label: 'Lucy'
+                    },
+                    {
+                      value: 'disabled',
+                      disabled: true,
+                      label: 'Disabled'
+                    },
+                    {
+                      value: 'Yiminghe',
+                      label: 'yiminghe'
+                    }
+                  ]}
+                ></Select>
+              </Form.Item>
+            </Row>
+          )}
         </Form>
       </AuthBtn>
       <div className="mt20">
