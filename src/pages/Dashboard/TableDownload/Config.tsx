e47beb8f55a23029bb156842cd024317bc33da10
jeffLang <foreverljj@outlook.com>
2022-12-26 17:20:24 +0800

    fix：报表下载列表报表名称和筛选条件优化

--- src/pages/Dashboard/TableDownload/Config.tsx
+++ src/pages/Dashboard/TableDownload/Config.tsx
@@ -59,15 +59,6 @@ export const columns = (args: OperateProps): ColumnsType<fileProps> => {
     },
     {
       title: '报表名称',
-      dataIndex: 'fileName',
-      ellipsis: true,
-      width: 180,
-      render: (text: String) => {
-        return text || UNKNOWN;
-      }
-    },
-    {
-      title: '筛选条件',
       dataIndex: 'filter',
       ellipsis: true,
       width: 160,
@@ -75,6 +66,33 @@ export const columns = (args: OperateProps): ColumnsType<fileProps> => {
         return record.reportName + ' ' + text;
       }
     },
+    // {
+    //   title: '报表名称',
+    //   dataIndex: 'fileName',
+    //   ellipsis: true,
+    //   width: 180,
+    //   render: (text: String) => {
+    //     return text || UNKNOWN;
+    //   }
+    // },
+    {
+      title: '筛选条件',
+      dataIndex: 'filter',
+      ellipsis: true,
+      width: 180,
+      render: (text: String) => {
+        return text || UNKNOWN;
+      }
+    },
+    // {
+    //   title: '筛选条件',
+    //   dataIndex: 'filter',
+    //   ellipsis: true,
+    //   width: 160,
+    //   render: (text, record) => {
+    //     return record.reportName + ' ' + text;
+    //   }
+    // },
     {
       title: '生成状态',
       dataIndex: 'status',
