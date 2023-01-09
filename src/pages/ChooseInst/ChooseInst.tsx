1f7e63216ac36ae0996baa8d50beac10ab5a2c64
jeff lang <langjinjie@tenacity.com.cn>
2023-01-09 15:25:57 +0800

    feat：选择机构/登录页面添加获取系统是否处于更新中判断

--- src/pages/ChooseInst/ChooseInst.tsx
+++ src/pages/ChooseInst/ChooseInst.tsx
@@ -3,20 +3,28 @@
  * @author Lester
  * @date 2021-09-28 14:48
  */
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Context } from 'src/store';
-import { queryInstList, chooseInst } from 'src/apis';
+import { queryInstList, chooseInst, requestGetMstatus } from 'src/apis';
 import { InstItem } from 'src/utils/interface';
 import style from './style.module.less';
 import { TOKEN_KEY } from 'src/utils/config';
+import Update from '../Update/Update';
 
 const colors: string[] = ['#76A2FA', '#7AC2FE', '#889BF0', '#91A9FF', '#7AD9FF', '#6BDAED'];
 
 const ChooseInst: React.FC<RouteComponentProps> = ({ history }) => {
   const { instList, setInstList } = useContext(Context);
+  const [updating, setUpdating] = useState(false);
 
   const getInstList = async () => {
+    // 判断是否处于系统更新中
+    const updateRes = await requestGetMstatus();
+    // 999 维护中
+    if (updateRes === '999') {
+      return setUpdating(true);
+    }
     const res: any = await queryInstList();
     if (Array.isArray(res)) {
       setInstList(res);
@@ -37,24 +45,29 @@ const ChooseInst: React.FC<RouteComponentProps> = ({ history }) => {
   }, []);
 
   return (
-    <div className={style.wrap}>
-      <section className={style.content}>
-        <h3 className={style.title}>选择机构进入</h3>
-        <ul className={style.instList}>
-          {instList.map((item: InstItem, index: number) => (
-            <li
-              key={item.corpId}
-              className={style.instItem}
-              style={{ backgroundColor: colors[index % 6] }}
-              onClick={() => handleChooseInst(item.corpId)}
-            >
-              <img className={style.instImg} src={item.logo} alt="" />
-              <div className={style.instName}>{item.corpName}</div>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </div>
+    <>
+      {updating || (
+        <div className={style.wrap}>
+          <section className={style.content}>
+            <h3 className={style.title}>选择机构进入</h3>
+            <ul className={style.instList}>
+              {instList.map((item: InstItem, index: number) => (
+                <li
+                  key={item.corpId}
+                  className={style.instItem}
+                  style={{ backgroundColor: colors[index % 6] }}
+                  onClick={() => handleChooseInst(item.corpId)}
+                >
+                  <img className={style.instImg} src={item.logo} alt="" />
+                  <div className={style.instName}>{item.corpName}</div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      )}
+      {updating && <Update />}
+    </>
   );
 };
 
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,19 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { Form, Input, Button } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 // import { getQueryParam } from 'tenacity-tools';
 import md5 from 'js-md5';
 import { Context } from 'src/store';
-import { login, queryUserInfo } from 'src/apis';
+import { login, queryUserInfo, requestGetMstatus } from 'src/apis';
 import style from './style.module.less';
 import { TOKEN_KEY } from 'src/utils/config';
+import Update from '../Update/Update';
 
 const { Password } = Input;
 const { Item } = Form;
 
 const Login: React.FC<RouteComponentProps> = ({ history }) => {
   const { setUserInfo, setIsMainCorp, setCurrentCorpId } = useContext(Context);
+  const [updating, setUpdating] = useState(false);
 
   const onSubmit = async ({ userName, password }: any) => {
     // @ts-ignore
@@ -31,35 +33,54 @@ const Login: React.FC<RouteComponentProps> = ({ history }) => {
     }
   };
 
+  // 获取系统状态是否处于更新中
+  const getMStatus = async () => {
+    // 判断是否处于系统更新中
+    const updateRes = await requestGetMstatus();
+    // 999 维护中
+    if (updateRes === '999') {
+      setUpdating(true);
+    }
+  };
+
+  useEffect(() => {
+    getMStatus();
+  }, []);
+
   return (
-    <div className={style.loginBg}>
-      <div className={style.loginWrap}>
-        <h3 className={style.title}>账号登录</h3>
-        <Form onFinish={onSubmit}>
-          <Item name="userName" rules={[{ required: true, message: '请输入账号' }]}>
-            <Input placeholder="账号" />
-          </Item>
-          <Item name="password" rules={[{ required: true, message: '请输入密码' }]}>
-            <Password
-              placeholder="密码"
-              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-            />
-          </Item>
-          <Item>
-            <Button type="primary" htmlType="submit">
-              登录
-            </Button>
-          </Item>
-        </Form>
-      </div>
-      <footer className={style.footer}>
-        <div className={style.copyright}>
-          本服务由年高提供。年高，面向金融领域提供产业应用方案。
-          <br />
-          Copyright @ 2021 Niangao All Rights Reserved
+    <>
+      {updating || (
+        <div className={style.loginBg}>
+          <div className={style.loginWrap}>
+            <h3 className={style.title}>账号登录</h3>
+            <Form onFinish={onSubmit}>
+              <Item name="userName" rules={[{ required: true, message: '请输入账号' }]}>
+                <Input placeholder="账号" />
+              </Item>
+              <Item name="password" rules={[{ required: true, message: '请输入密码' }]}>
+                <Password
+                  placeholder="密码"
+                  iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                />
+              </Item>
+              <Item>
+                <Button type="primary" htmlType="submit">
+                  登录
+                </Button>
+              </Item>
+            </Form>
+          </div>
+          <footer className={style.footer}>
+            <div className={style.copyright}>
+              本服务由年高提供。年高，面向金融领域提供产业应用方案。
+              <br />
+              Copyright @ 2021 Niangao All Rights Reserved
+            </div>
+          </footer>
         </div>
-      </footer>
-    </div>
+      )}
+      {updating && <Update />}
+    </>
   );
 };
 
